9a633bae99fa00a3662b41867181894592daa646
Taylor Laine <tlaine@msudenver.edu>
2017-03-08 19:07:10 -0700

    created a little game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// create cards array with queen and king 
+var cards = ["queen", "queen", "king", "king"]; 
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+	
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	 alert("You found a match!"); 
+	} else {
+		alert("Try Again!!");
+	}
+} 
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+console.log("User flipped " + cardTwo);
