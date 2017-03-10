e17cb855a7759e4f1201e1edccdf81f94eb55182
Taylor Laine <tlaine@msudenver.edu>
2017-03-09 18:37:45 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -3,20 +3,22 @@
 var cards = ["queen", "queen", "king", "king"]; 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+	    	alert("You found a match!");
+	  	} else {
+	    	alert("Sorry, try again.");
+	 	}
+	 }
+};
 
-if (cardsInPlay.length === 2) {
-	
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-	 alert("You found a match!"); 
-	} else {
-		alert("Try Again!!");
-	}
-} 
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+flipCard(0);
+flipCard(2);
